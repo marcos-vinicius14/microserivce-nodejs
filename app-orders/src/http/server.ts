deb55523d320cbecd41619536a335ad35cd739e2
marcos <marcos.vinicius.dev3@gmail.com>
2025-09-14 09:10:00 -0300

    feat: setup server and dockerfille

---
+++ app-orders/src/http/server.ts
@@ -0,0 +1,39 @@
+import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
+import { z } from 'zod';
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod';
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: '*', // Em produção, restrinja para o seu domínio: 'https://meu-dominio.com'
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.post('/orders', {
+  schema: { 
+    body: z.object({
+      amount: z.number(),
+    }),
+  },
+}, (request, reply) => { 
+  const { amount } = request.body;
+  console.log(`[ORDERS] Creating order with amount: ${amount}`);
+  
+  return reply.status(201).send({ message: 'Order created' });
+});
+
+
+app.get('/health', (request, reply) => {
+  return reply.send('OK');
+});
+
+app.listen({ host: '0.0.0.0', port: 3333 }).then(() => {
+  console.log('[ORDERS] Server listening on http://localhost:3333');
+});
